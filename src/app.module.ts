b3ca447a8ffb84176088d497a01dbb519ff82d0d
jeffrey-kor <wjdrlrkdl3@gmail.com>
2021-10-31 16:48:32 +0900

    controller/init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './core/users/presentation/users.controller';
 import { AdminController } from './core/admin/presentation/admin.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot()],
   controllers: [UsersController, AdminController],
   providers: [],
 })
-export class AppModule {}
+  export class AppModule {}
--- src/core/users/presentation/users.controller.ts
+++ src/core/users/presentation/users.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get('/test')
+  async test(): Promise<string> {
+    return '미라님 시작합시다. 1월달까지 달려봐요.';
+  }
+}
